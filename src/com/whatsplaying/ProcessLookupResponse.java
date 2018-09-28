67ed9205bf72ab8db25a165d28965618015d00fe
Kurtis Mash <Kurtis.Mash@bbc.co.uk>
2018-09-28 18:49:45 +0100

    Remove unused declarations.

--- src/com/whatsplaying/ProcessLookupResponse.java
+++ src/com/whatsplaying/ProcessLookupResponse.java
@@ -1,19 +1,22 @@
 package com.whatsplaying;
 
 class ProcessLookupResponse implements IProcessLookupResponse {
-	Track previousTrack = new Track(null, null, null, 0);
+  Track previousTrack = new Track(null, null, null, 0);
 
-	@Override
-	public void processLookupResponse(Track[] tracks, TempFile file) { 
-		file.delete();
+  @Override
+  public void processLookupResponse(Track[] tracks, TempFile file) {
+    file.delete();
 
-		//Although multiple tracks may be returned, here we are only caring about the first track returned.
-			if( tracks[0].getTrackName().equals(previousTrack.getTrackName()) && tracks[0].getAlbumArtist().equals(previousTrack.getAlbumArtist()) ) {
-				//Ignore as same track still playing
-			} else {
-				System.out.println( "{ \"song\" : \"" + tracks[0].getTrackName() + "\", \"artist\" : \"" + tracks[0].getAlbumArtist() + "\" }" );
-				previousTrack = tracks[0];
-			}
-	}
+    // Although multiple tracks may be returned, here we are only caring about the first track
+    // returned.
+    if (tracks[0].getTrackName().equals(previousTrack.getTrackName())
+        && tracks[0].getAlbumArtist().equals(previousTrack.getAlbumArtist())) {
+      // Ignore as same track still playing
+    } else {
+      System.out.println("{ \"song\" : \"" + tracks[0].getTrackName() + "\", \"artist\" : \""
+          + tracks[0].getAlbumArtist() + "\" }");
+      previousTrack = tracks[0];
+    }
+  }
 
 }
