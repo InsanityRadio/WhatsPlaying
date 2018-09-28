e8e83c81b1068d36a5dbcf43bc861230ca61e7cd
Kurtis Mash <Kurtis.Mash@bbc.co.uk>
2018-09-28 19:20:17 +0100

    Fix #2.
    Implemented a "previousPublished" check to see what's been published. If the new track matches the previous published, it will not publish. If the new track and previous track match but not the previousPublishedTrack, it will publish.

--- src/com/whatsplaying/ProcessLookupResponse.java
+++ src/com/whatsplaying/ProcessLookupResponse.java
@@ -2,18 +2,24 @@ package com.whatsplaying;
 
 class ProcessLookupResponse implements IProcessLookupResponse {
 	Track previousTrack = new Track(null, null, null, 0);
+	Track previousPublishedTrack = new Track(null, null, null, 0);
 
 	@Override
 	public void processLookupResponse(Track[] tracks, TempFile file) { 
 		file.delete();
-
-		//Although multiple tracks may be returned, here we are only caring about the first track returned.
-			if( tracks[0].getTrackName().equals(previousTrack.getTrackName()) && tracks[0].getAlbumArtist().equals(previousTrack.getAlbumArtist()) ) {
+		// Although multiple tracks may be returned, here we are only caring about the first track returned.
+		
+			if( tracks[0].getTrackName().equals(previousPublishedTrack.getTrackName()) && tracks[0].getAlbumArtist().equals(previousPublishedTrack.getAlbumArtist()) ) {
 				//Ignore as same track still playing
-			} else {
-				System.out.println( "{ \"song\" : \"" + tracks[0].getTrackName() + "\", \"artist\" : \"" + tracks[0].getAlbumArtist() + "\" }" );
-				previousTrack = tracks[0];
+			} else if( tracks[0].getTrackName().equals(previousTrack.getTrackName()) && tracks[0].getAlbumArtist().equals(previousTrack.getAlbumArtist()) ){
+				publishTrack(tracks[0]);
 			}
+			previousTrack = tracks[0];
+	}
+	
+	void publishTrack(Track track) {
+		previousPublishedTrack = track;
+		System.out.println( "{ \"song\" : \"" + track.getTrackName() + "\", \"artist\" : \"" + track.getAlbumArtist() + "\" }" );
 	}
 
 }
