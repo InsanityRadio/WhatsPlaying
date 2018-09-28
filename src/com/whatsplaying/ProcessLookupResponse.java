f49f210a6cd325b580d346f6d235e9b8cd3e205f
Kurtis Mash <Kurtis.Mash@bbc.co.uk>
2018-09-28 18:32:52 +0100

    Remove duplicate consecutive output.
    Now checks that detected track and artist are different to the previosuly detected track and artist before publishing.

--- src/com/whatsplaying/ProcessLookupResponse.java
+++ src/com/whatsplaying/ProcessLookupResponse.java
@@ -1,15 +1,19 @@
 package com.whatsplaying;
 
 class ProcessLookupResponse implements IProcessLookupResponse {
+	Track previousTrack = new Track(null, null, null, 0);
 
 	@Override
 	public void processLookupResponse(Track[] tracks, TempFile file) { 
 		file.delete();
-		for(int i=0; i<tracks.length; i++) {
-			Track track = tracks[i];
-			System.out.println( "{ \"song\" : \"" + track.getTrackName() + "\", \"artist\" : \"" + track.getAlbumArtist() + "\" }" );
-		}
-		
+
+		//Although multiple tracks may be returned, here we are only caring about the first track returned.
+			if( tracks[0].getTrackName().equals(previousTrack.getTrackName()) && tracks[0].getAlbumArtist().equals(previousTrack.getAlbumArtist()) ) {
+				//Ignore as same track still playing
+			} else {
+				System.out.println( "{ \"song\" : \"" + tracks[0].getTrackName() + "\", \"artist\" : \"" + tracks[0].getAlbumArtist() + "\" }" );
+				previousTrack = tracks[0];
+			}
 	}
-	
+
 }
